c163f2f1e2ff1dd4b2b09f61cc6ae2f0cfa2d1e9
Iago Mosqueira <iago@mosqueira.eu>
2018-05-02 15:29:43 +0200

    Added some global variables

--- R/readSS3.R
+++ R/readSS3.R
@@ -6,7 +6,7 @@
 #
 # Distributed under the terms of the European Union Public Licence (EUPL) V.1.1.
 
-utils::globalVariables(".")
+utils::globalVariables(c(".", "data", "vars"))
 
 # readFLBFss3 {{{
 
